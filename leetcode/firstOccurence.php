d546fa47e794bbe782c62ab471ab6cf6d9084062
alarefcodes <alarefabdo1@gmail.com>
2023-09-12 15:48:34 +0200

    first occurence solution with php

---
+++ leetcode/firstOccurence.php
@@ -0,0 +1,14 @@
+<?php
+class Solution {
+
+    /**
+     * @param String $haystack
+     * @param String $needle
+     * @return Integer
+     */
+    function strStr($haystack, $needle) {
+        $pos = strpos($haystack, $needle);
+        return $pos === false ? -1 : $pos;
+    }
+    }
+?>
