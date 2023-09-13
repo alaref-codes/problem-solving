d016e06247eec77ae03ff05bd4f69c2560287869
alarefcodes <alarefabdo1@gmail.com>
2023-09-13 15:15:23 +0200

    majorityElement solution in php

---
+++ leetcode/majorityElement.php
@@ -0,0 +1,26 @@
+class Solution {
+
+/**
+ * @param Integer[] $nums
+ * @return Integer
+ */
+function majorityElement($nums) {
+    $result = 0;
+    $unique = array_values(array_unique($nums)); 
+    for ($i = 0; $i < count($unique); $i++) {   
+        $counter = 0;
+        foreach ($nums as &$value) {
+            if ($unique[$i] === $value){
+                $counter += 1;
+            }
+        }
+        $check  = count($nums) / 2;
+        if ($counter > $check) {
+            $result = $unique[$i];
+            break;
+        }             
+    }
+
+    return $result;
+}
+}
